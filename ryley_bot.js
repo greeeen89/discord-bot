d3e808b7fff6f7444ad39c6156e93fb5ab67dd04
Ryley Oroku <greeeenmango89@gmail.com>
2021-01-15 17:55:20 -0800

    First commit

---
+++ ryley_bot.js
@@ -0,0 +1,47 @@
+// here is a file
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+var yesNoQuestionWords = ["are", "is", "does", "have", "can", "am", "was",
+                          "were", "has", "do", "did", "could*", "should", "may",
+                          "would", "shall", "will", "won't", "wont"];
+var questionWordsRegex = new RegExp(yesNoQuestionWords.join("|"), "i");
+var yesNoAnswers = ["Better not tell you.", "Most likely.", "Very doubtful.",
+                    "Ask again.", "As I see it, yes.", "My sources say no.",
+                    "Cannot predict now.", "Yes.", "Don't count on it.", "Without a doubt",
+                    "Fuck off.", "Don't talk to me.", "Don't you have better things to do?"];
+
+// bot startup
+client.on('ready', () => {
+  console.log("Connectd as: " + client.user.tag);
+})
+
+// respond to messages
+client.on('message', (receivedMessage) => {
+  var contentLowercase = receivedMessage.content.toLowerCase();
+  if(receivedMessage.author == client.user) {
+    return;
+  }
+
+  if(contentLowercase.includes("hello ryley")) {
+    receivedMessage.channel.send("Hi " + receivedMessage.author.toString() + " !")
+  }
+
+  if(receivedMessage.content.toLowerCase().includes("shower")) {
+    receivedMessage.channel.send("I'm not in the shower");
+  }
+
+  // respond to yes or no questions
+  if(contentLowercase.includes("?")) {
+    if(questionWordsRegex.test(contentLowercase)) { // look for key words
+      receivedMessage.channel.send(yesNoAnswers[getRandInt(yesNoAnswers.length)], {tts: true});
+    }
+  }
+})
+
+bot_secret_token = "NTk0NzY5NjAwMzg2OTU3MzE0.XRhRXw.MKTYi_H5TzieyHWcfMuNsCECt0Y";
+client.login(bot_secret_token);
+
+function getRandInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
